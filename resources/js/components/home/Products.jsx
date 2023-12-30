b83600dac20e99c44f044649584ff13dfaa9c594
KowshickChowdhury <kowshick1170@gmail.com>
2023-12-30 11:24:53 +0600

    update

--- resources/js/components/home/Products.jsx
+++ resources/js/components/home/Products.jsx
@@ -8,13 +8,13 @@ export default function Products() {
         <div className='grid md:grid-cols-2 gap-4'>
             <div className='bg-slate-200 text-center'>
                 <img src='/img/gallery/4.png' alt="" className='mb-5' />
-                <h1 className='text-xl'>e-Manager - a business management software</h1>
+                <h1 className='text-xl font-semibold'>e-Manager - a business management software</h1>
                 <p className={`${styles.paragraph}`}>Create Invoice/Bill, Automated Report, Get Stock Report</p>
                 <button className='bg-secondary text-white inline-block mt-2 mb-7 px-4 py-2 rounded-md'><a href="https://e-manager.org/">Visit Now</a></button>
             </div>
             <div className='bg-slate-200 text-center'>
                 <img src='/img/gallery/2.png' alt="" className='mb-5' />
-                <h1 className='text-xl'>Free QR Code Generator - FlexQR</h1>
+                <h1 className='text-xl font-semibold'>Free QR Code Generator - FlexQR</h1>
                 <p className={`${styles.paragraph}`}>create QR for your website, information and company profile.</p>
                 <button className='bg-secondary text-white inline-block mt-2 mb-7 px-4 py-2 rounded-md'><a href="https://qr.devsbrain.com/">Visit Now</a></button>
             </div>
